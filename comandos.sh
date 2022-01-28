5652cd00a93cdd24cd152c46de475bb4b4b2069e
hamttaro09 <enzomaiben@gmail.com>
2022-01-28 09:46:57 -0300

    comandos definitive edition

--- comandos.sh
+++ comandos.sh
@@ -2,20 +2,23 @@ sudo pacman -S git
 sudo pacman -S bspwm sxhkd nitrogen alacritty arandr picom neofetch htop 
 
 sudo pacman -S --needed git base-devel
+cd
 git clone https://aur.archlinux.org/visual-studio-code-bin.git
 cd visual-studio-code-bin
 makepkg -si
 
-git clone https://github.com/hamttaro09/entorno.git
+mkdir ~/.config/bspwm
+mkdir ~/.config/sxhkd
 
-cp /Documentos/entorno/bspwm ~/.config/bspwm
-cp /Documentos/entorno/sxhkd ~/.config/sxhkd
+cp -r ~/Documentos/entorno/bspwm ~/.config/
+cp -r ~/Documentos/entorno/sxhkd ~/.config/
 chmod +x ~/.config/bspwm/bspwmrc 
 chmod +x ~/.config/bspwm/scripts/bspwm_resize
 
 sudo pacman -S yay
 
 yay -S polybar
+cd 
 mkdir .config/polybar 
 cp /usr/share/doc/polybar/config ~/.config/polybar 
 cp ~/Documentos/entorno/launch.sh ~/.config/polybar 
@@ -24,27 +27,31 @@ echo '~/.config/polybar/./launch.sh' >> ~/.config/bspwm/bspwmrc
 
 sudo pacman -Sy rofi
 
-sudo reboot 
-
+sudo su 
 usermod --shell /usr/bin/zsh hmt
 usermod --shell /usr/bin/zsh root
 chown hmt:hmt /root
 chown hmt:hmt /root/.cache -R
 chown hmt:hmt /root/.local -R
 
-sudo su && cd 
+sudo su
+cd 
 yay -S --noconfirm zsh-theme-powerlevel10k-git
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-cd /home/hmt/Documentos/entorno 
-sudo cp .zshrc /home/hmt /root
+
+
+su hmt
+cd
+yay -S --noconfirm zsh-theme-powerlevel10k-git
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 
 pacman -S bat
 pacman -S lsd
+cd /home/hmt/Documentos/entorno 
+sudo cp  .zshrc /home/hmt /root
+sudo cp  .zshrc /root
 
-su hmt 
-yay -S --noconfirm zsh-theme-powerlevel10k-git
-echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 
 
